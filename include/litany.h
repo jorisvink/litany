c1cd8420813a2aa4c9741278937a4f916db26b19
Joris Vink <joris@coders.se>
2025-05-19 11:54:52 +0200

    macos: portability macros for endian crap

--- include/litany.h
+++ include/litany.h
@@ -28,6 +28,16 @@
 		}							\
 	} while (0)
 
+#if defined(__APPLE__)
+#include <libkern/OSByteOrder.h>
+#define htobe16(x)		OSSwapHostToBigInt16(x)
+#define htobe32(x)		OSSwapHostToBigInt32(x)
+#define htobe64(x)		OSSwapHostToBigInt64(x)
+#define be16toh(x)		OSSwapBigToHostInt16(x)
+#define be32toh(x)		OSSwapBigToHostInt32(x)
+#define be64toh(x)		OSSwapBigToHostInt64(x)
+#endif
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
